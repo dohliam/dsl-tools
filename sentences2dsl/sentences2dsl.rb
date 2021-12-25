922e39901f59e25df173adfbf7f2b36866c3d6ca
dohliam <dohliam@gmail.com>
2021-12-25 15:06:55 -0800

    sentences2dsl - refactor

--- sentences2dsl/sentences2dsl.rb
+++ sentences2dsl/sentences2dsl.rb
@@ -11,78 +11,56 @@ require 'fileutils'
 require 'optparse'
 require 'json'
 
+require_relative '../lib/dsl_lib.rb'
+
+def format_dictionary(sentences_file, options)
+  dict_content = ""
+  lang = options[:stoplist]
+  json = read_stoplist(options, lang)
+
+  sentences_file.each_line do |sentence|
+    lang1,lang2 = sentence.chomp.split("\t")
+    lang1_array = []
+    lang1.split(" ").each do |w|
+      word = w.gsub(/[\?\!\.,:;"“”]/, "")
+      lang1_array.push(word.downcase)
+    end
+    lang1_array.each do |keyword|
+      if !json.include?(keyword) && !keyword.match(/^[\s\n]*$/)
+        dict_content << keyword + "\n"
+      end
+    end
+    dict_content << "\t" + lang1 + "\n"
+    dict_content << "\t" + lang2 + "\n\n"
+  end
+  dict_content
+end
+
 options = {}
 OptionParser.new do |opts|
-  opts.banner = "Usage: example.rb [options]"
+  opts.banner = "Usage: sentences2dsl.rb [options] [filename]"
 
   opts.on('-d', '--debug', 'Output dictionary text to stdin (without creating compressed file)') { options[:debug] = true }
   opts.on('-f', '--from-lang LANG', 'Name of source language') { |v| options[:from_lang] = v }
+  opts.on('-m', '--monolingual', 'Only provide unidirectional lookups (default is bidirectional)') { options[:monolingual] = true }
   opts.on('-n', '--dict-name NAME', 'Full name of dictionary') { |v| options[:dict_name] = v }
   opts.on('-t', '--to-lang LANG', 'Name of target language') { |v| options[:to_lang] = v }
   opts.on('-s', '--stoplist LANG', 'Specify a stoplist language to filter keywords') { |v| options[:stoplist] = v }
+  opts.on('-S', '--stop-dir DIR', 'Specify path of stoplist directory') { |v| options[:stopdir] = v }
 
 end.parse!
 
 if ARGV[0]
-  @dict_source = ARGV[0]
+  dict_source = ARGV[0]
 else
   puts "  Please enter a target sentence file."
   exit
 end
 sentences_file = File.read(dict_source)
 
-# configure the location of your stoplist directory here
-stoplist_dir = ""
+header_stuff = get_header(options)
+dsl_name = get_dict_name(dict_source)
+header_stuff = get_header(options)
+dict_content = format_dictionary(sentences_file, options)
 
-lang = ""
-json = []
-
-if options[:stoplist]
-  lang = options[:stoplist]
-  stopwords_file = File.read(stoplist_dir + lang + ".json")
-  json = JSON.parse(stopwords_file)
-end
-
-dict_name = "Bilingual dictionary"
-if !options[:dict_name]
-  dict_name = options[:dict_name]
-end
-index_lang = ""
-contents_lang = ""
-
-header_stuff = "#NAME \"#{dict_name}\"\n#INDEX_LANGUAGE \"#{index_lang}\"\n#CONTENTS_LANGUAGE \"#{contents_lang}\"\n\n"
-
-dict_content = ""
-
-sentences_file.each_line do |sentence|
-  lang1,lang2 = sentence.chomp.split("\t")
-  lang1_array = []
-  lang1.split(" ").each do |w|
-    word = w.gsub(/[\?\!\.,:;"“”]/, "")
-    lang1_array.push(word.downcase)
-  end
-  lang1_array.each do |keyword|
-    if !json.include?(keyword) && !keyword.match(/^[\s\n]*$/)
-      dict_content << keyword + "\n"
-    end
-  end
-  dict_content << "\t" + lang1 + "\n"
-  dict_content << "\t" + lang2 + "\n\n"
-end
-
-if !options[:debug]
-  dsl_name = File.basename(dict_source, File.extname(dict_source))
-
-  File.open(dsl_name + ".dsl", "wt", encoding: "UTF-16LE") do |f|
-    f << header_stuff
-    f << dict_content
-  end
-
-  `dictzip #{dsl_name}.dsl`
-
-  puts "Done! Your dictionary is now available in the file #{dsl_name}.dsl.dz"
-  exit
-end
-
-puts header_stuff
-puts dict_content
+handle_output(options, dsl_name, header_stuff, dict_content)
